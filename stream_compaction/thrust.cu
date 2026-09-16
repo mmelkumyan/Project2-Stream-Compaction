9dffa751bbaff6be82efe930678679e50a40cb8b
Mark Melkumyan <mmelkumyan@users.noreply.github.com>
2026-09-15 22:31:15 -0400

    Completes thrust scan

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -18,11 +18,27 @@ namespace StreamCompaction {
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            // Copy data to GPU
+            int *dev_odata, *dev_idata;
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            
+            // Create device pts
+            thrust::device_ptr<int> dev_thrust_idata(dev_idata);
+            thrust::device_ptr<int> dev_thrust_odata(dev_odata);
+
+            // Call thrusts's scan
             timer().startGpuTimer();
-            // TODO use `thrust::exclusive_scan`
-            // example: for device_vectors dv_in and dv_out:
-            // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::exclusive_scan(dev_thrust_idata, dev_thrust_idata + n, dev_thrust_odata);
             timer().endGpuTimer();
+            
+            // Copy to host
+            cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Cleanup
+            cudaFree(dev_odata);
+            cudaFree(dev_idata);
         }
     }
 }
